63bef20899a4c2d56d41c7c30cb34c7a9afbf6c6
Dabeer Mirza <DabeerM@hotmail.com>
2020-03-05 15:11:48 +0000

    We now have some ambient music in the simulation so that the simulation feels less mechanical

--- ArmillarySphere/Assets/Scripts/TurnOnOffTutorial.cs
+++ ArmillarySphere/Assets/Scripts/TurnOnOffTutorial.cs
@@ -6,18 +6,18 @@ public class TurnOnOffTutorial : MonoBehaviour
 {
 
     // To control whether or not you can see the video
-    private bool video_on = true;
+    [SerializeField] bool video_on; 
 
     // The video gameobject
     [SerializeField] GameObject video_player;
     
     public void ToggleVideo()
     {
-        // Make the video active/inactive
-        video_player.SetActive(video_on);
-
         // So the video ready to be destroyed/created as appropriate
         // you press the simulate button
         video_on = !video_on;
+
+        // Make the video active/inactive
+        video_player.SetActive(video_on);
     }
 }
